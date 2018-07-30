de583caef9d95eb655ca31279d25f1200f84f4e5
Benny <benny@BennydeMacBook-Pro.local>
2018-07-30 09:06:08 +0800

    refine gitignore, add all .el, remove custom.el

---
+++ lisp/bn-helm.el
@@ -0,0 +1,37 @@
+;; helm config
+;;
+;; helm
+(require 'helm)
+(require 'helm-config)
+;; setq 
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(setq helm-M-x-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+(setq helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match    t)
+
+(setq helm-split-window-in-side-p           t
+      helm-move-to-line-cycle-in-source     t
+      helm-ff-search-library-in-sexp        t
+      helm-scroll-amount                    8
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+;; set key
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-c h f") 'helm-find)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h m") 'helm-mark-ring)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(helm-autoresize-mode 1)
+(helm-mode 1)
+
+;; helm config end
