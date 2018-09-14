a9e814744557d2b95b57a3567537e67b3ce68adb
Benny Song <bigknife@outlook.com>
2018-09-14 18:49:45 +0800

    better line-spacing

--- lisp/bn-looknfeel.el
+++ lisp/bn-looknfeel.el
@@ -32,7 +32,27 @@
 (global-set-key (kbd "C-c C-k") 'windmove-down)
 
 ;; line space
-(setq-default line-spacing 0.5)
+;; (setq-default line-spacing 0)
+(defvar line-padding 2)
+(defun add-line-padding ()
+  "Add extra padding between lines"
+
+; remove padding overlays if they already exist
+(let ((overlays (overlays-at (point-min))))
+  (while overlays
+    (let ((overlay (car overlays)))
+      (if (overlay-get overlay 'is-padding-overlay)
+          (delete-overlay overlay)))
+    (setq overlays (cdr overlays))))
+; add a new padding overlay
+(let ((padding-overlay (make-overlay (point-min) (point-max))))
+  (overlay-put padding-overlay 'is-padding-overlay t)
+  (overlay-put padding-overlay 'line-spacing (* .1 line-padding))
+  (overlay-put padding-overlay 'line-height (+ 1 (* .1 line-padding))))
+(setq mark-active nil))
+
+
+(add-hook 'buffer-list-update-hook 'add-line-padding)
 
 ;; fold-this
 ;; (global-set-key (kbd "C-c C-f") 'fold-this-all)
